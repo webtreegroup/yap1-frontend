3657a89a08336adca4e3e15442d8b3bc6167b63e
webtree <web-tree@yandex.ru>
2020-12-07 14:45:16 +0400

    поправил server.ts

--- server.ts
+++ server.ts
@@ -3,7 +3,7 @@ import express from 'express'
 const app: express.Application = express()
 const PORT = 4000
 
-app.use(express.static('./'))
+app.use(express.static('./static'))
 
 app.listen(PORT, () => {
     console.log(`Start in ${PORT}!`)
--- static/server.js
+++ static/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 const app = express();
 const PORT = 4000;
-app.use(express.static('./'));
+app.use(express.static('./static'));
 app.listen(PORT, () => {
     console.log(`Start in ${PORT}!`);
 });
