d19d0ab37333f321694bdcf580b93bb0fc983c65
webtree <web-tree@yandex.ru>
2020-12-10 13:45:30 +0400

    правки ревью - удалил SIGNIN_FORM_CONTROLS из констант чата

--- src/pages/Chats/Chats.consts.ts
+++ src/pages/Chats/Chats.consts.ts
@@ -1,16 +1,4 @@
-import { IInputControl } from "../../components/InputControl/InputControl.model.js"
 import { IChatMessage } from "./components/ChatMessage/ChatMessage.model.js"
-
-
-export const SIGNIN_FORM_CONTROLS: IInputControl[] = [
-    { name: 'email', label: 'Почта', inputType: 'email' },
-    { name: 'login', label: 'Логин' },
-    { name: 'first_name', label: 'Имя' },
-    { name: 'second_name', label: 'Фамилия' },
-    { name: 'phone', label: 'Телефон' },
-    { name: 'password', label: 'Пароль', inputType: 'password' },
-    { name: 'password_confirm', label: 'Пароль (ещё раз)', inputType: 'password' },
-]
 
 export const MESSAGES: IChatMessage[] = [
     {
--- static/src/pages/Chats/Chats.consts.js
+++ static/src/pages/Chats/Chats.consts.js
@@ -1,12 +1,3 @@
-export const SIGNIN_FORM_CONTROLS = [
-    { name: 'email', label: 'Почта', inputType: 'email' },
-    { name: 'login', label: 'Логин' },
-    { name: 'first_name', label: 'Имя' },
-    { name: 'second_name', label: 'Фамилия' },
-    { name: 'phone', label: 'Телефон' },
-    { name: 'password', label: 'Пароль', inputType: 'password' },
-    { name: 'password_confirm', label: 'Пароль (ещё раз)', inputType: 'password' },
-];
 export const MESSAGES = [
     {
         check: true,
