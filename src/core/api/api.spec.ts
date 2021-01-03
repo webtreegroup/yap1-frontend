395da76708edb538986f2bfa9743beb189bf58a1
webtree <web-tree@yandex.ru>
2021-01-03 22:59:18 +0400

    доработки тестов

--- src/core/api/api.spec.ts
+++ src/core/api/api.spec.ts
@@ -6,48 +6,95 @@ import chaiHttp from 'chai-http'
 
 chai.use(chaiHttp)
 
-const agent = chai.request.agent(API_BASE_PATH)
+const agent = chai.request.agent(API_BASE_PATH).keepOpen()
 
 const { expect } = chai
 
+function logout() {
+    agent.get('/auth/logout')
+}
+
 describe("Api usage suite", () => {
-    it("It should get all chats, after signin", () => {
+    it("It should signin", (done) => {
         agent
             .post('/auth/signin')
-            .send({ login: 'SanchoPansoYo', password: 'SanchoPansoYo123' })
-            .then(function (res: request.Response) {
+            .send({ login: 'test1', password: 'test1' })
+            .end(function (_: any, res: request.Response) {
+                console.log(res.status)
                 // @ts-ignore
                 expect(res).to.have.cookie('authCookie')
-
-                return agent
-                    .get('/chats')
-                    .then(function (res: request.Response) {
-                        // @ts-ignore
-                        expect(res).to.have.status(200)
-                        expect(res).to.be.an('array')
-                        agent.close()
-                    })
+                // @ts-ignore
+                expect(res).to.have.status(200)
+                done()
             })
     })
 
-    it("List of chats should be update, after signin", () => {
-        agent
-            .post('/auth/signin')
-            .send({ login: 'SanchoPansoYo', password: 'SanchoPansoYo123' })
-            .then(function (res: request.Response) {
-                // @ts-ignore
-                expect(res).to.have.cookie('authCookie')
+    // it("It should get all chats, after signin", (done) => {
+    //     agent
+    //         .get('/chats')
+    //         .end(function (_:any, res: request.Response) {
+    //             // @ts-ignore
+    //             expect(res).to.have.status(200)
+    //             expect(res.body).to.be.an('array')
+    //             done()
+    //         })
+    // })
 
-                return agent
-                    .post('/chats')
-                    .send({ title: String(Date.now()) })
-                    .then(function (res: request.Response) {
-                        // @ts-ignore
-                        expect(res).to.have.status(200)
-                        expect(res).to.not.have.property('id')
-                        agent.close()
-                    })
-            })
-    })
+    // it("User profile should be update, after signin", (done) => {
+    //     agent
+    //         .put('/user/profile')
+    //         .send({
+    //             first_name: "test1 test 6",
+    //             second_name: String(Date.now()),
+    //             display_name: "ZumerEpt",
+    //             login: "test1",
+    //             email: "test@test.com",
+    //             phone: "79173333333"}
+    //         )
+    //         .end(function (_: any, res: request.Response) {
+    //             console.log(res.body)
+    //             // @ts-ignore
+    //             expect(res).to.have.status(200)
+    //             expect(res.body).to.have.property('id')
+    //             done()
+    //         })
+    // })
+
+    // it("It should be an 404 error, if API address not valid", (done) => {
+    //     agent
+    //         .get('/chats-not-valid')
+    //         .then(function (res: request.Response) {
+    //             // @ts-ignore
+    //             expect(res).not.have.status(200)
+                
+    //             throw res
+    //         })
+    //         .catch(function (err: request.Response) {
+    //             // @ts-ignore
+    //             expect(err).to.have.status(404)
+    //             done()
+    //         })
+    // })
+
+    // it("It should be an 400 error, if API request not valid", (done) => {
+    //     agent
+    //         .post('/chats')
+    //         .send({ titleNotValid: '' })
+    //         .then(function (res: request.Response) {
+    //             // console.log(res.status)
+    //             // @ts-ignore
+    //             expect(res).to.have.status(200)
+                
+    //             throw res
+    //         })
+    //         .catch(function (err: request.Response) {
+    //             // console.log(err)
+    //             // @ts-ignore
+    //             expect(err).not.have.status(200)
+    //             done()
+    //         })
+    // })
 })
+
+agent.close(logout)
 
