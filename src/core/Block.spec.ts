afd73283c593ecb81a16ba8ba85c0f87128b367a
webtree <web-tree@yandex.ru>
2021-01-03 14:54:50 +0400

    дописал тесты для шаблонизатора

--- src/core/Block.spec.ts
+++ src/core/Block.spec.ts
@@ -1,7 +1,7 @@
 import chai from 'chai'
 import { IComponent } from '../App.types.js'
 import { render } from '../utils/common.utils.js'
-import { Block } from './Block.js'
+import { Block, IBaseTemplateRender, IBlockChildren } from './Block.js'
 
 const { expect } = chai
 
@@ -10,9 +10,24 @@ interface IBlockComponent extends IComponent {
 }
 
 describe("Block usage suite", () => {
-    function createBlock(props: IBlockComponent) {
-        return new Block<HTMLDivElement, IBlockComponent>('div', props)
+    function createBlock(
+        props: IBlockComponent,
+        children = {} as IBlockChildren, 
+        baseTmplRender?: IBaseTemplateRender
+    ) {
+        return new Block<HTMLDivElement, IBlockComponent>(
+            'div', 
+            props,
+            children,
+            baseTmplRender
+        )
     }
+
+    const className = 'block-component'
+
+    const tmplRender = () => `
+        <div className="block-component__body" data-component="root"></div>
+    `
 
     it("Props should be change", () => {
         const Block = createBlock({ desc: 'description' })
@@ -23,20 +38,29 @@ describe("Block usage suite", () => {
     })
 
     it("Block should be hide", () => {
-        const Block = createBlock({ className: 'block-component' })
+        const Block = createBlock({ className })
         render('body', Block)
 
         Block.hide()
 
-        const blockNode = document.querySelector('.block-component')
+        const blockNode = document.querySelector(`.${className}`)
         expect(blockNode).to.eql(null)
     })
 
     it("Block should be show", () => {
-        const Block = createBlock({ className: 'block-component' })
+        const Block = createBlock({ className })
         render('body', Block)
 
-        const blockNode = document.querySelector('.block-component')
+        const blockNode = document.querySelector(`.${className}`)
         expect(blockNode).not.eql(null)
+    })
+
+    it("Block should be render like specify in template", () => {
+        const Block1 = createBlock({ className: 'child-block-component' })
+        const Block2 = createBlock({ className }, { root: Block1 }, tmplRender)
+        render('body', Block2)
+
+        const childBlockNode = document.querySelector('.child-block-component')
+        expect(childBlockNode).not.eql(null)
     })
 })
--- static/src/core/Block.spec.js
+++ static/src/core/Block.spec.js
@@ -3,9 +3,13 @@ import { render } from '../utils/common.utils.js';
 import { Block } from './Block.js';
 const { expect } = chai;
 describe("Block usage suite", () => {
-    function createBlock(props) {
-        return new Block('div', props);
+    function createBlock(props, children = {}, baseTmplRender) {
+        return new Block('div', props, children, baseTmplRender);
     }
+    const className = 'block-component';
+    const tmplRender = () => `
+        <div className="block-component__body" data-component="root"></div>
+    `;
     it("Props should be change", () => {
         const Block = createBlock({ desc: 'description' });
         Block.setProps({ desc: 'description edit' });
@@ -13,16 +17,23 @@ describe("Block usage suite", () => {
         expect(Block.props).not.eql({ desc: 'description' });
     });
     it("Block should be hide", () => {
-        const Block = createBlock({ className: 'block-component' });
+        const Block = createBlock({ className });
         render('body', Block);
         Block.hide();
-        const blockNode = document.querySelector('.block-component');
+        const blockNode = document.querySelector(`.${className}`);
         expect(blockNode).to.eql(null);
     });
     it("Block should be show", () => {
-        const Block = createBlock({ className: 'block-component' });
+        const Block = createBlock({ className });
         render('body', Block);
-        const blockNode = document.querySelector('.block-component');
+        const blockNode = document.querySelector(`.${className}`);
         expect(blockNode).not.eql(null);
+    });
+    it("Block should be render like specify in template", () => {
+        const Block1 = createBlock({ className: 'child-block-component' });
+        const Block2 = createBlock({ className }, { root: Block1 }, tmplRender);
+        render('body', Block2);
+        const childBlockNode = document.querySelector('.child-block-component');
+        expect(childBlockNode).not.eql(null);
     });
 });
