afa08c2776a56d9846e5864fb4f503973fd1905b
webtree <web-tree@yandex.ru>
2021-01-03 23:44:49 +0400

    доработал тесты для АПИ

--- static/src/core/api/api.spec.js
+++ static/src/core/api/api.spec.js
@@ -8,13 +8,13 @@ const { expect } = chai;
 function logout() {
     agent.get('/auth/logout');
 }
+logout();
 describe("Api usage suite", () => {
     it("It should signin", (done) => {
         agent
             .post('/auth/signin')
-            .send({ login: 'test1', password: 'test1' })
+            .send({ login: 'test3', password: 'test3' })
             .end(function (_, res) {
-            console.log(res.status);
             // @ts-ignore
             expect(res).to.have.cookie('authCookie');
             // @ts-ignore
@@ -22,65 +22,62 @@ describe("Api usage suite", () => {
             done();
         });
     });
-    // it("It should get all chats, after signin", (done) => {
-    //     agent
-    //         .get('/chats')
-    //         .end(function (_:any, res: request.Response) {
-    //             // @ts-ignore
-    //             expect(res).to.have.status(200)
-    //             expect(res.body).to.be.an('array')
-    //             done()
-    //         })
-    // })
-    // it("User profile should be update, after signin", (done) => {
-    //     agent
-    //         .put('/user/profile')
-    //         .send({
-    //             first_name: "test1 test 6",
-    //             second_name: String(Date.now()),
-    //             display_name: "ZumerEpt",
-    //             login: "test1",
-    //             email: "test@test.com",
-    //             phone: "79173333333"}
-    //         )
-    //         .end(function (_: any, res: request.Response) {
-    //             console.log(res.body)
-    //             // @ts-ignore
-    //             expect(res).to.have.status(200)
-    //             expect(res.body).to.have.property('id')
-    //             done()
-    //         })
-    // })
-    // it("It should be an 404 error, if API address not valid", (done) => {
-    //     agent
-    //         .get('/chats-not-valid')
-    //         .then(function (res: request.Response) {
-    //             // @ts-ignore
-    //             expect(res).not.have.status(200)
-    //             throw res
-    //         })
-    //         .catch(function (err: request.Response) {
-    //             // @ts-ignore
-    //             expect(err).to.have.status(404)
-    //             done()
-    //         })
-    // })
-    // it("It should be an 400 error, if API request not valid", (done) => {
-    //     agent
-    //         .post('/chats')
-    //         .send({ titleNotValid: '' })
-    //         .then(function (res: request.Response) {
-    //             // console.log(res.status)
-    //             // @ts-ignore
-    //             expect(res).to.have.status(200)
-    //             throw res
-    //         })
-    //         .catch(function (err: request.Response) {
-    //             // console.log(err)
-    //             // @ts-ignore
-    //             expect(err).not.have.status(200)
-    //             done()
-    //         })
-    // })
+    it("It should get all chats, after signin", (done) => {
+        agent
+            .get('/chats')
+            .end(function (_, res) {
+            // @ts-ignore
+            expect(res).to.have.status(200);
+            expect(res.body).to.be.an('array');
+            done();
+        });
+    });
+    it("User profile should be update, after signin", (done) => {
+        agent
+            .put('/user/profile')
+            .send({
+            first_name: "test3",
+            second_name: String(Date.now()),
+            display_name: "ZumerEpt",
+            login: "test3",
+            email: "test3@test3.ru",
+            phone: "333333333"
+        })
+            .end(function (_, res) {
+            // @ts-ignore
+            expect(res).to.have.status(200);
+            expect(res.body).to.have.property('id');
+            done();
+        });
+    });
+    it("It should be an 404 error, if API address not valid", (done) => {
+        agent
+            .get('/chats-not-valid')
+            .then(function (res) {
+            // @ts-ignore
+            expect(res).not.have.status(200);
+            throw res;
+        })
+            .catch(function (err) {
+            // @ts-ignore
+            expect(err).to.have.status(404);
+            done();
+        });
+    });
+    it("It should be an 400 error, if API request not valid", (done) => {
+        agent
+            .post('/chats')
+            .send({ titleNotValid: '' })
+            .then(function (res) {
+            // @ts-ignore
+            expect(res).to.not.have.status(200);
+            throw res;
+        })
+            .catch(function (err) {
+            // @ts-ignore
+            expect(err).not.have.status(200);
+            done();
+        });
+    });
 });
 agent.close(logout);
