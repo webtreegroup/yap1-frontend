0e380d57d56e9e3e4ccc042a1b5a7c95c7b86bd1
webtree <web-tree@yandex.ru>
2021-01-16 22:44:49 +0400

    интеграция месседжей - neTiskat, neTrogat

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
-    watch: false,
+    watch: true,
     mode: 'development',
     entry: {
         index: './src/App.ts',
