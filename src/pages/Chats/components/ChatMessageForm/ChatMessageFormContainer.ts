bedf1cffc271b470742739c007a9a8020ae3eeef
webtree <web-tree@yandex.ru>
2021-01-17 11:08:07 +0400

    добавил логику очистки формы

--- src/pages/Chats/components/ChatMessageForm/ChatMessageFormContainer.ts
+++ src/pages/Chats/components/ChatMessageForm/ChatMessageFormContainer.ts
@@ -3,8 +3,14 @@ import { loaderOffAction, loaderOnAction } from 'core/store'
 import { ChatMessageForm } from './ChatMessageForm'
 
 export class ChatMessageFormContainer {
+    form: ChatMessageForm
+
     constructor() {
         this.onMessageSend = this.onMessageSend.bind(this)
+
+        this.form = new ChatMessageForm({
+            onMessageSend: this.onMessageSend,
+        })
     }
 
     onMessageSend(request: IChatMessage): void {
@@ -12,13 +18,12 @@ export class ChatMessageFormContainer {
 
         // TODO: тут будем юзать WebSocket
         console.log(request)
+        this.form.element?.reset()
 
         loaderOffAction()
     }
 
     createBlock(): ChatMessageForm {
-        return new ChatMessageForm({
-            onMessageSend: this.onMessageSend,
-        })
+        return this.form
     }
 }
