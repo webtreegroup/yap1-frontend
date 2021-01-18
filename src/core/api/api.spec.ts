1a1fa2e617c4d9d5117a437024f3c2d63c20e120
webtree <web-tree@yandex.ru>
2021-01-18 21:47:53 +0400

    еще один тест на апи

--- src/core/api/api.spec.ts
+++ src/core/api/api.spec.ts
@@ -1,4 +1,4 @@
-import { ChatAPI } from '.';
+import { API_BASE_PATH, ChatAPI } from '.'
 
 function mockFetch(status: number, data?: any) {
     const xhrMockObj = {
@@ -8,25 +8,53 @@ function mockFetch(status: number, data?: any) {
         readyState: 4,
         status,
         response: data,
-    };
+    }
 
-    const xhrMockClass = () => xhrMockObj;
+    const xhrMockClass = () => xhrMockObj
 
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
-    window.XMLHttpRequest = jest.fn().mockImplementation(xhrMockClass);
+    window.XMLHttpRequest = jest.fn().mockImplementation(xhrMockClass)
 
     setTimeout(() => {
         // eslint-disable-next-line @typescript-eslint/ban-ts-comment
         // @ts-ignore
-        xhrMockObj.onload();
-    }, 0);
+        xhrMockObj.onload()
+    }, 0)
+
+    return xhrMockObj
 }
 
-mockFetch(200, 'fuck you');
+describe('API usage suite', () => {
+    it('it should return array of chats', async () => {
+        const chats = [
+            {
+                id: 1,
+                title: 'name',
+                avatar: 'url',
+            },
+        ]
 
-it('it should by done maza facka', async () => {
-    const res = await ChatAPI.request().then((xhr) => xhr.response);
+        mockFetch(200, JSON.stringify(chats))
 
-    expect(res).toEqual('fuck you');
+        const response = await ChatAPI.request().then((xhr) => JSON.parse(xhr.response))
+
+        expect(response).toEqual(chats)
+    })
+
+    it('it should to be called with specific arguments', async () => {
+        const xhrMock = mockFetch(200)
+
+        await ChatAPI.getChatUsers(1)
+
+        expect(xhrMock.open).toBeCalledWith('GET', `${API_BASE_PATH}/chats/1/users`, true)
+    })
+
+    it('it should to be 400 error, when pass wrong arguments', async () => {
+        mockFetch(400)
+
+        const response = await ChatAPI.getChatUsers(0)
+
+        expect(response.status).toEqual(400)
+    })
 })
