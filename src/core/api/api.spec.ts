5ac6c5a379595da4d8cceb110acd9209e85ac04b
webtree <web-tree@yandex.ru>
2021-01-18 20:11:22 +0400

    временный тесты апи

--- src/core/api/api.spec.ts
+++ src/core/api/api.spec.ts
@@ -1,14 +1,4 @@
-function performRequest(callback: any) {
-    const xhr = new XMLHttpRequest()
-    xhr.open('GET', 'https://fffff.com/')
-    xhr.onreadystatechange = () => {
-        if (xhr.readyState !== 4 || xhr.status !== 200) return
-        callback(xhr.response)
-    }
-    xhr.responseType = 'json'
-    xhr.setRequestHeader('Accept', 'application/json')
-    xhr.send(null)
-}
+import { API_BASE_PATH, ChatAPI } from '.'
 
 describe('request', () => {
     const xhrMock: Partial<XMLHttpRequest> = {
@@ -20,14 +10,52 @@ describe('request', () => {
         response: 'Hello World!',
     }
 
-    jest.spyOn(window, 'XMLHttpRequest').mockImplementation(() => xhrMock as XMLHttpRequest)
-
-    const callback = jest.fn()
-
-    performRequest(callback)
-
     it('her', () => {
-        (xhrMock.onreadystatechange as EventListener)(new Event(''))
-        expect(callback.mock.calls).toEqual([['Hello World!']])
+        jest.spyOn(window, 'XMLHttpRequest').mockImplementation(() => xhrMock as XMLHttpRequest)
+        const callback = jest.fn()
+
+        ChatAPI.request().then((xhr) => {
+            callback(xhr.response)
+
+            // // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // // @ts-ignore
+            // xhrMock.onload()
+            expect(callback.mock.calls).toEqual([['Hello World!']])
+        })
     })
 })
+
+// function performRequest(callback: any) {
+//     const xhr = new XMLHttpRequest()
+//     xhr.open('GET', 'https://example.com/')
+//     xhr.onreadystatechange = () => {
+//         if (xhr.readyState !== 4 || xhr.status !== 200) return
+//         callback(xhr.response)
+//     }
+//     xhr.responseType = 'json'
+//     xhr.setRequestHeader('Accept', 'application/json')
+//     xhr.send(null)
+// }
+
+// describe('request', () => {
+//     const xhrMock: Partial<XMLHttpRequest> = {
+//         open: jest.fn(),
+//         send: jest.fn(),
+//         setRequestHeader: jest.fn(),
+//         readyState: 4,
+//         status: 200,
+//         response: 'Hello World!',
+//     }
+
+//     jest.spyOn(window, 'XMLHttpRequest').mockImplementation(() => xhrMock as XMLHttpRequest)
+
+//     const callback = jest.fn()
+
+//     performRequest(callback)
+
+//     it('her', () => {
+//         expect(xhrMock.open).toBeCalledWith('GET', 'https://example.com/');
+//         (xhrMock.onreadystatechange as EventListener)(new Event(''))
+//         expect(callback.mock.calls).toEqual([['Hello World!']])
+//     })
+// })
